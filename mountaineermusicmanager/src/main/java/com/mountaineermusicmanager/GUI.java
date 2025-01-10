811d758e9373ec808c00459e626cda49dc6f805a
Nevinkf <nevinkf@proton.me>
2025-01-10 16:35:57 -0700

    Beginning switch to javafx

--- mountaineermusicmanager/src/main/java/com/mountaineermusicmanager/GUI.java
+++ mountaineermusicmanager/src/main/java/com/mountaineermusicmanager/GUI.java
@@ -2,105 +2,110 @@
  * File Name: GUI.java
  * Author: Nevin Fullerton
  * Date: 1/7/2025
- * Description:
+ * Description: 
  */
 
 package com.mountaineermusicmanager;
-import java.awt.BorderLayout;
-import java.util.LinkedList;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.ListSelectionModel;
-import javax.swing.table.DefaultTableModel;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-import javafx.scene.media.MediaPlayer;; // Make music playing its own class
 
-public class GUI extends JFrame {
+public class GUI extends Application {
 
-    final private JPanel mainPanel; // Display content to user
-    final private JPanel playListPanel; // Display playLists or other sections the user can view
-    final private JPanel mediaPanel; // Allows user to play and pause current songs, along with other functions
+    // final private JPanel mainPanel; // Display content to user
+    // final private JPanel playListPanel; // Display playLists or other sections the user can view
+    // final private JPanel mediaPanel; // Allows user to play and pause current songs, along with other functions
 
-    private JScrollPane tableScrollPane; // Holds the songTable and allows user to scroll in the table
-    private JTable songTable;
-    private LinkedList<String> songsDisplayed = new LinkedList<>(); 
+    // private JScrollPane tableScrollPane; // Holds the songTable and allows user to scroll in the table
+    // private JTable songTable;
+    // private LinkedList<String> songsDisplayed = new LinkedList<>(); 
 
-    private MediaPlayer test;
-    
+    // final private SongPlayer songPlayer;    
 
-    public GUI() {
-        this.setLayout(new BorderLayout());
+    // public GUI() {        
+        
 
-        mainPanel = new JPanel();
+    //     songPlayer = new SongPlayer();
 
-        songTable = new JTable();
 
-        tableScrollPane = new JScrollPane(songTable);
+    //     this.setLayout(new BorderLayout());
 
-        displayTable();
+    //     mainPanel = new JPanel();
 
-        mainPanel.add(tableScrollPane);
+    //     songTable = new JTable();
 
-        this.add(mainPanel, BorderLayout.CENTER);
+    //     tableScrollPane = new JScrollPane(songTable);
 
-        playListPanel = new JPanel();
+    //     displayTable();
 
-        JLabel testLabel2 = new JLabel("playListPanel");
-        playListPanel.add(testLabel2);
+    //     mainPanel.add(tableScrollPane);
 
-        this.add(playListPanel, BorderLayout.WEST);
+    //     this.add(mainPanel, BorderLayout.CENTER);
 
-        mediaPanel = new JPanel();
+    //     playListPanel = new JPanel();
 
-        JLabel testLabel3 = new JLabel("mediaPanel");
-        mediaPanel.add(testLabel3);
+    //     JLabel testLabel2 = new JLabel("playListPanel");
+    //     playListPanel.add(testLabel2);
 
-        this.add(mediaPanel, BorderLayout.NORTH);
+    //     this.add(playListPanel, BorderLayout.WEST);
 
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.pack();
-    }
+    //     mediaPanel = new JPanel();
 
-    /*
-     * 
-     */
-    public void displayTable() {
-        String[][] testData = { // TODO remove and implement data to be variable
-            {"1", "2", "3"}, 
-            {"4", "5","6"},
-            {"7", "8", "9"},
-            {"7", "8", "9"},
-            {"7", "8", "9"},
-            {"7", "8", "9"},
-            {"7", "8", "9"}
-        };
+    //     JButton playButton = new JButton("Play");
+    //     playButton.addActionListener((ActionEvent e) -> {
+    //         songPlayer.playSong("songs\\Heaven Pierce Her - ULTRAKILL- INFINITE HYPERDEATH - 01 The Fire Is Gone (for Piano, Saxophone and Trumpet).mp3");
+    //     });
 
-        String[] columnNames = {"Title", "Author", "Genre"};
+    //     mediaPanel.add(playButton);
 
-        songTable.setModel(new DefaultTableModel(testData, columnNames));
-        songTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        songTable.setDefaultEditor(Object.class, null);
+    //     this.add(mediaPanel, BorderLayout.NORTH);
 
-        refreshGUI();
-    }
+    //     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //     this.pack();
+    // }
 
-    /*
-     * 
-     */
-    private void refreshGUI() {
-        this.revalidate();
-        this.repaint();
+    // /*
+    //  * 
+    //  */
+    // public void displayTable() {
+    //     String[][] testData = { // TODO remove and implement data to be variable
+    //         {"1", "2", "3"}, 
+    //         {"4", "5","6"},
+    //         {"7", "8", "9"},
+    //         {"7", "8", "9"},
+    //         {"7", "8", "9"},
+    //         {"7", "8", "9"},
+    //         {"7", "8", "9"}
+    //     };
+
+    //     String[] columnNames = {"Title", "Author", "Genre"};
+
+    //     songTable.setModel(new DefaultTableModel(testData, columnNames));
+    //     songTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+    //     songTable.setDefaultEditor(Object.class, null);
+
+    //     refreshGUI();
+    // }
+
+    // /*
+    //  * 
+    //  */
+    // private void refreshGUI() {
+    //     this.revalidate();
+    //     this.repaint();
+    // }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
     }
 
     public static void main(String[] args) {
         GUI programGUI = new GUI();
-        programGUI.setVisible(true);
+        // programGUI.setVisible(true);
     }
-
 
     // Get Song metadata and display it in table, then button to play song
 
--- mountaineermusicmanager/src/main/java/com/mountaineermusicmanager/SongPlayer.java
+++ mountaineermusicmanager/src/main/java/com/mountaineermusicmanager/SongPlayer.java
@@ -2,11 +2,33 @@
  * File Name: SongPlayer.java
  * Author: Nevin Fullerton
  * Date: 1/9/2025
- * Description:
+ * Description: Plays and mangaes songs
  */
 
- package com.mountaineermusicmanager;
+package com.mountaineermusicmanager;
+import java.io.File;
+
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class SongPlayer {
+
+    private MediaPlayer songPlayer;
+
+    SongPlayer() {
+        StackPane test = new StackPane();
+        
+
+    }
+
+    /*
+     * 
+     */
+    public void playSong(String fileName) {
+        Media songFile = new Media(new File(fileName).toURI().toString());
+        songPlayer = new MediaPlayer(songFile);
+        songPlayer.play();
+    }
 
 }
